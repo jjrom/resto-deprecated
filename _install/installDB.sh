60bbb82eb3c1bbf6235131c34d0b013547c0056c
jerome.gasperi@cnes.fr <>
2014-04-23 10:09:30 +0200

    Embed SQL code within installDB.sh to avoid 'psql -f' error (which apparently not work on RedHat linux through Virtual Machine...!)

--- _install/installDB.sh
+++ _install/installDB.sh
@@ -42,7 +42,6 @@ DROPFIRST=NO
 DB=resto
 USER=resto
 ADMIN=sresto
-SQL=`dirname $0`/resto.sql
 usage="## RESTo database installation\n\n  Usage $0 -d <PostGIS directory> -P <sresto (Read+Write database) user password> -p <resto (Read-Only database) user password> [-s <database SUPERUSER> -F]\n\n  -d : absolute path to the directory containing postgis.sql\n  -s : dabase SUPERUSER (default "postgres")\n  -F : WARNING - suppress existing admin schema within resto database\n"
 while getopts "d:s:p:P:hF" options; do
     case $options in
@@ -98,7 +97,105 @@ DROP SCHEMA admin CASCADE;
 EOF
 fi
 #############CREATE DB ##############
-psql -d $DB -U $SUPERUSER -f $SQL
+psql -d $DB -U $SUPERUSER << EOF
+
+-- hstore is used for collection datasources
+CREATE EXTENSION hstore;
+
+-- 
+-- admin schema contains history and collections tables
+--
+CREATE SCHEMA admin;
+
+--
+-- history table stores all user requests
+--
+CREATE TABLE admin.history (
+    gid                 SERIAL PRIMARY KEY,
+    service             VARCHAR(10),
+    collection          VARCHAR(50),
+    query               TEXT DEFAULT NULL,
+    realquery           TEXT DEFAULT NULL,
+    querytime           TIMESTAMP,
+    url                 TEXT DEFAULT NULL,
+    ip                  VARCHAR(15),
+    userid              VARCHAR(255) DEFAULT 'anonymous'
+);
+CREATE INDEX idx_service_history ON admin.history (service);
+CREATE INDEX idx_userid_history ON admin.history (userid);
+
+
+--
+-- collections table list all RESTo collections
+--
+CREATE TABLE admin.collections (
+    collection          VARCHAR(50) PRIMARY KEY,
+    creationdate        TIMESTAMP,
+    controller          VARCHAR(50) DEFAULT 'DefaultController',
+    theme               VARCHAR(50),
+    status              VARCHAR(10) DEFAULT 'public',
+    dbname              VARCHAR(10) DEFAULT 'resto',
+    hostname            VARCHAR(50) DEFAULT 'localhost',
+    port                VARCHAR(5)  DEFAULT '5432',
+    schemaname          VARCHAR(20) NOT NULL,
+    tablename           VARCHAR(20) DEFAULT 'products',
+    configuration       TEXT
+);
+CREATE INDEX idx_status_collections ON admin.collections (status);
+CREATE INDEX idx_creationdate_collections ON admin.collections (creationdate);
+
+--
+-- osdescriptions table describe all RESTo collections
+--
+CREATE TABLE admin.osdescriptions (
+    collection          VARCHAR(50),
+    lang                VARCHAR(2),
+    shortname           VARCHAR(50),
+    longname            VARCHAR(255),
+    description         TEXT,
+    tags                TEXT,
+    developper          VARCHAR(50),
+    contact             VARCHAR(50),
+    query               VARCHAR(255),
+    attribution         VARCHAR(255)
+);
+ALTER TABLE ONLY admin.osdescriptions ADD CONSTRAINT fk_collection FOREIGN KEY (collection) REFERENCES admin.collections(collection);
+ALTER TABLE ONLY admin.osdescriptions ADD CONSTRAINT cl_collection UNIQUE(collection, lang);
+CREATE INDEX idx_collection_osdescriptions ON admin.osdescriptions (collection);
+CREATE INDEX idx_lang_osdescriptions ON admin.osdescriptions (lang);
+
+--
+-- tags table list all tags attached to data within collection
+--
+CREATE TABLE admin.tags (
+    tag                 VARCHAR(50) PRIMARY KEY,
+    creationdate        TIMESTAMP,
+    updateddate         TIMESTAMP,
+    occurence           INTEGER
+);
+CREATE INDEX idx_updated_tags ON admin.tags (updateddate);
+
+--
+-- users table list user informations
+--
+CREATE TABLE admin.users (
+    userid              VARCHAR(255) PRIMARY KEY,  -- should be an email adress
+    groups              TEXT, -- group names are comma separated
+    username            VARCHAR(50) NOT NULL,
+    password            VARCHAR(50) NOT NULL, -- stored as md5
+    registrationdate    TIMESTAMP NOT NULL,
+    lastsessionid       VARCHAR(255)
+);
+
+--
+-- rights table list user rights on collection
+--
+CREATE TABLE admin.rights (
+    groupid             VARCHAR(255),  -- group name as stored in admin.users groups column
+    collection          VARCHAR(50) NOT NULL, -- same as collection in admin.collections
+    rights              TEXT -- serialized json representation of services rights
+);
+EOF
 
 # Rights
 psql -U $SUPERUSER -d $DB << EOF
