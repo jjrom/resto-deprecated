67ebe8436ee05947a2440233071b65331b42d00a
jerome.gasperi@cnes.fr <>
2014-05-28 16:14:53 +0200

    Remove unusued swipebox from build

--- _install/buildJavascriptAndCSS.sh
+++ _install/buildJavascriptAndCSS.sh
@@ -20,7 +20,7 @@ cp -R js/mapshup/theme/default/img ../js/css
 echo ' ==> Pack javascript dependencies files to dependencies.js single file'
 cat js/mjquery/mjquery.js > ../js/dependencies.js
 cat js/mjquery/mjquery.ui.js >> ../js/dependencies.js
-cat js/swipebox/js/jquery.swipebox.min.js >> ../js/dependencies.js
+#cat js/swipebox/js/jquery.swipebox.min.js >> ../js/dependencies.js
 cat js/history/jquery.history.js >> ../js/dependencies.js
 cat js/visible/jquery.visible.min.js >> ../js/dependencies.js
 cat js/mol/OpenLayers.js >> ../js/dependencies.js
@@ -39,14 +39,14 @@ cat js/mol/theme/default/style.css > ../js/css/dependencies.css
 cat js/mjquery/mjquery.css >> ../js/css/dependencies.css
 cat js/mapshup/theme/default/mapshup.css >> ../js/css/dependencies.css
 cat js/foundation/foundation.min.css >> ../js/css/dependencies.css
-cat js/swipebox/css/swipebox.min.css >> ../js/css/dependencies.css
+#cat js/swipebox/css/swipebox.min.css >> ../js/css/dependencies.css
 cat js/fontawesome/css/font-awesome.min.css >> ../js/css/dependencies.css
 
 echo ' ==> Compress dependencies.css file with Yahoo! CSS compressor'
 java -jar _compressors/css_compressor.jar ../js/css/dependencies.css > ../js/css/dependencies.min.css
 rm -Rf ../js/css/dependencies.css
 
-echo ' ==> Copy images to ../js/img/'
-cp -R js/swipebox/img/* ../js/css/img
+#echo ' ==> Copy images to ../js/img/'
+#cp -R js/swipebox/img/* ../js/css/img
 
 
